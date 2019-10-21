7ec9ca84ee04276888e3de87dd3cf35e8ed07e9f
Ondrej Hliba <hliba.ondra@live.com>
2019-10-21 12:06:34 +0200

    Wire up Menu buttons

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -31,7 +31,7 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.btnHelp = new System.Windows.Forms.Button();
             this.btnSettings = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStart
@@ -43,6 +43,7 @@
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Spustit hru";
             this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
             // 
             // btnHelp
             // 
@@ -53,6 +54,7 @@
             this.btnHelp.TabIndex = 1;
             this.btnHelp.Text = "Pomoc";
             this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.BtnHelp_Click);
             // 
             // btnSettings
             // 
@@ -63,23 +65,25 @@
             this.btnSettings.TabIndex = 2;
             this.btnSettings.Text = "Nastavení";
             this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.BtnSettings_Click);
             // 
-            // button1
+            // btnEnd
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(12, 201);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(167, 57);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Konec";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnEnd.Location = new System.Drawing.Point(12, 201);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(167, 57);
+            this.btnEnd.TabIndex = 3;
+            this.btnEnd.Text = "Konec";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.BtnEnd_Click);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(194, 267);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEnd);
             this.Controls.Add(this.btnSettings);
             this.Controls.Add(this.btnHelp);
             this.Controls.Add(this.btnStart);
@@ -94,7 +98,7 @@
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnHelp;
         private System.Windows.Forms.Button btnSettings;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEnd;
     }
 }
 
--- Menu.cs
+++ Menu.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using vetsibere.Properties;
 
 namespace vetsibere
 {
@@ -15,6 +16,33 @@ namespace vetsibere
         public Menu()
         {
             InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            SwitchForms(new Game());
+        }
+
+        private void BtnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Větší karta bere. Nuda.");
+        }
+
+        private void BtnSettings_Click(object sender, EventArgs e)
+        {
+            SwitchForms(new Settings());
+        }
+
+        private void BtnEnd_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void SwitchForms(Form form)
+        {
+            form.Show();
+            form.FormClosed += (sender, args) => Show();
+            Hide();
         }
     }
 }
