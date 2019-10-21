ac5ffe303ce2ab178b0bfb4e14a39cc1f0427eea
Ondrej Hliba <hliba.ondra@live.com>
2019-10-21 13:24:28 +0200

    Add Card class

---
+++ Card.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace vetsibere
+{
+    class Card
+    {
+        private Size _size = new Size(40, 120);
+        public Point Position = new Point(0, 0);
+        public int Value { get; set; } = -1;
+        public string ImagePath { get; set; } = Application.StartupPath + "\\Images\\";
+
+        public Card() { }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawRectangle(Pens.Black, new Rectangle(Position, _size));
+        }
+    }
+}
