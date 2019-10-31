0f9c08faf97fefd49f70108ce0f56b78a920633a
Adam Otradovec <adam.otradovec@seznam.cz>
2019-10-31 08:39:12 +0100

    Removed card type and name label

--- Card.Designer.cs
+++ Card.Designer.cs
@@ -28,49 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblName = new System.Windows.Forms.Label();
             this.pbImage = new System.Windows.Forms.PictureBox();
-            this.lblValue = new System.Windows.Forms.Label();
             this.lblOwner = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
             this.SuspendLayout();
             // 
-            // lblName
-            // 
-            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblName.Location = new System.Drawing.Point(0, 0);
-            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(200, 28);
-            this.lblName.TabIndex = 0;
-            this.lblName.Text = "cardName";
-            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // pbImage
             // 
-            this.pbImage.Location = new System.Drawing.Point(0, 105);
-            this.pbImage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pbImage.Location = new System.Drawing.Point(0, 35);
+            this.pbImage.Margin = new System.Windows.Forms.Padding(4);
             this.pbImage.Name = "pbImage";
-            this.pbImage.Size = new System.Drawing.Size(200, 172);
+            this.pbImage.Size = new System.Drawing.Size(200, 242);
             this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbImage.TabIndex = 1;
             this.pbImage.TabStop = false;
             // 
-            // lblValue
-            // 
-            this.lblValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblValue.Location = new System.Drawing.Point(4, 73);
-            this.lblValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblValue.Name = "lblValue";
-            this.lblValue.Size = new System.Drawing.Size(192, 28);
-            this.lblValue.TabIndex = 2;
-            this.lblValue.Text = "value";
-            this.lblValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // lblOwner
             // 
             this.lblOwner.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lblOwner.Location = new System.Drawing.Point(0, 28);
+            this.lblOwner.Location = new System.Drawing.Point(0, 3);
             this.lblOwner.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblOwner.Name = "lblOwner";
             this.lblOwner.Size = new System.Drawing.Size(200, 28);
@@ -84,22 +60,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.Controls.Add(this.lblOwner);
-            this.Controls.Add(this.lblValue);
             this.Controls.Add(this.pbImage);
-            this.Controls.Add(this.lblName);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Card";
-            this.Size = new System.Drawing.Size(200, 277);
+            this.Size = new System.Drawing.Size(200, 278);
             ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.PictureBox pbImage;
-        private System.Windows.Forms.Label lblValue;
         private System.Windows.Forms.Label lblOwner;
     }
 }
